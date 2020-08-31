7ca6295ed328ad80bcad414daca304a16b0c4b87
sho-zy <j.shoji.0118@gmail.com>
2020-08-31 11:50:14 +0900

    リファクタ

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,19 @@
 const functions = require('firebase-functions')
-const express = require('express')
+const app = require('express')()
 const { Nuxt } = require('nuxt')
-const app = express()
 const nuxt = new Nuxt({
   dev: false,
   buildDir: '.nuxt',
-  build: {
-    publicPath: '/'
-  }
- })
-function handler(req, res) {
-  nuxt.renderRoute('/', { req }).then(result => {
-    res.send(result.html)
-  }).catch(e => {
+  build: { publicPath: '/' },
+})
+const handler = async (req, res) => {
+  try {
+    const { html } = await nuxt.renderRoute('/', { req })
+    res.send(html)
+  } catch (e) {
     res.send(e)
-  })
+  }
 }
 
 app.use(handler)
-exports.ssrapp = functions.https.onRequest(app)
+exports.ssrapp = functions.https.onRequest(app) // リージョン変更NG (リライトできない)
